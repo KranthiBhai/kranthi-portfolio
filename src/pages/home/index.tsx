c30b2865fed45bf229dbff33a3a433e2658d24d7
KranthiBhai <kk232243@gmail.com>
2022-03-08 22:08:15 +0530

    homepage fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { DetailsCard } from '../../components/DetailsCard'
 import Shell from '../../components/Shell'
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
-          <div>
-            <Shell >
-              <div className="w-[40%] h-full flex items-center justify-end bg-[#C36A2D]">
-              <DetailsCard />
-              </div>
-            </Shell>
+    <div>
+      <Shell >
+        <div className=' w-full h-full flex overflow-hidden'>
+          <div className="w-[40%] h-full flex pt-20 justify-end bg-[#C36A2D]">
+            <DetailsCard />
           </div>
+          <div className=' w-[60%] relative left-40 px-2 pt-20'>
+            <h1 className=' text-[4rem] font-extrabold'>Hello</h1>
+            <p className=' text-2xl'>Here's who I am & what I do</p>
+            <div className=' flex items-center gap-x-4 my-4'>
+              <button onClick={() => navigate('/resume')} className=' px-2 py-1 bg-blue-800 text-xl text-white rounded-lg'>Resume</button>
+              <button onClick={() => navigate('/projects')} className=' px-2 py-1 border rounded-lg text-xl hover:bg-blue-800 hover:text-white hover:border-none border-black '>Projects</button>
+            </div>
+            <p className=' w-1/2'>
+              I am currently working as a <strong>React Developer</strong> in <strong>SqubeSoftSol.pvt.ltd</strong> company from july/14/22. My strength is the HTML & CSS , I can work any kind of project in HTML & CSS. I have good experiance and expert in css frameworks like Tailwindcss, Radix-Ui, Material-Ui, Bootstrap etc.,
+            </p>
+          </div>
+        </div>
+      </Shell>
+    </div>
   )
 }
